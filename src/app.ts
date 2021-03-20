cedfa05f5568d8fd1443d874eafa2306f26c3c63
Markus Wolf <mail@markus-wolf.de>
2021-03-20 13:55:41 +0100

    feat: add pull request merged hook

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import type { Probot } from "probot";
 
 import { onLabelAdded } from "./label-added";
+import { onPullRequestMerged } from "./pull-request-merged";
 
 export function app(app: Probot): void {
   app.on("pull_request.labeled", onLabelAdded);
+  app.on("pull_request.merged", onPullRequestMerged);
 }
