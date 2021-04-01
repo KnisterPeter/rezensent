da161c915db8579cf869e716a712e28743788b39
Markus Wolf <mail@markus-wolf.de>
2021-04-01 20:28:08 +0200

    refactor: simplify pull request types

--- src/github/pr.ts
+++
@@ -1,5 +0,0 @@
-import { Endpoints } from "@octokit/types";
-
-export type PullRequest = Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}"]["response"]["data"];
-export type PullRequests = Endpoints["GET /repos/{owner}/{repo}/pulls"]["response"]["data"] &
-  { state: "open" | "closed" }[];
