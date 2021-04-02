f38fda432e9208b343f21bdbb57521958844b7a0
Markus Wolf <mail@markus-wolf.de>
2021-04-02 15:30:24 +0200

    feat: move the managing of review prs into a task
    Moving the review handling in a task as well does the following:
    
    - align the code base
    - wait for updates on currently scheduled tasks
    - no concurrent updates of the handled pull requests
    
    Also this contains a better heuristic to detect manual commits on
    review branches.

--- src/event/synchronize.ts
+++ src/event/synchronize.ts
@@ -1,23 +1,16 @@
 import type { EventTypesPayload, WebhookEvent } from "@octokit/webhooks";
 import type { Context } from "probot";
 
-import { withGit } from "../github/clone";
-import { getPullRequestCommits } from "../github/commits";
 import { match, PullRequestBase } from "../pr/matcher";
 import { enqueue } from "../tasks/queue";
 import { synchronizeManaged } from "../tasks/synchronize_managed";
+import { synchronizeReview } from "../tasks/synchronize_review";
 
 export async function onPullRequestUpdated(
   context: EventTypesPayload["pull_request.synchronize"] &
     Omit<Context<any>, keyof WebhookEvent<any>>
 ) {
-  const {
-    number,
-    head: { ref: head, sha: headSha },
-    merged,
-    state,
-    labels,
-  } = context.payload.pull_request;
+  const { number, merged, state, labels } = context.payload.pull_request;
 
   if (merged) {
     // ignore already merged branch updates
@@ -42,84 +35,10 @@ export async function onPullRequestUpdated(
     },
 
     async review(review) {
-      context.log.debug(`[${review}] update is team review request`);
-
-      const managedPullRequest = await review.parent();
-
-      const { data: commit } = await context.octokit.git.getCommit(
-        context.repo({
-          commit_sha: headSha,
-        })
-      );
-
-      const managedTimeline = await context.octokit.paginate(
-        context.octokit.issues.listEventsForTimeline,
-        context.repo({
-          mediaType: {
-            previews: ["mockingbird"],
-          },
-          issue_number: managedPullRequest.number,
-          per_page: 100,
-        })
-      );
-      const referencedCommits = managedTimeline
-        .filter((item) => item.event === "referenced")
-        .map((item) => item.commit_id);
-
-      if (referencedCommits.includes(commit.sha)) {
-        context.log.debug(
-          `[${review}] Update is referenced in managed pull request; keep commit`
-        );
-        return;
-      }
-
-      context.log.debug(
-        `[${review}] invalid commit ${commit.sha} onto team-pr; reset and cherry-pick onto PR-${managedPullRequest.number}`
-      );
-
-      const commits = await getPullRequestCommits(context, review.number);
-
-      await withGit(
+      enqueue(
         context,
-        {
-          branch: head,
-          depth: commits.length + 1,
-        },
-        async (git) => {
-          context.log.debug(
-            { commits },
-            `[${review}] Commits (reset to HEAD^ now)`
-          );
-
-          await git.resetHardCommits();
-          await git.forcePush(head);
-
-          try {
-            const newCommitId = await git.cherryPick({
-              commit: commit.sha,
-              onto: managedPullRequest.head.ref,
-            });
-            await context.octokit.issues.createComment(
-              context.repo({
-                issue_number: review.number,
-                body: `Invalid commit on review pull request! We reset the branch!
-
-    We cherry-picked your commit ${commit.sha} as ${newCommitId} onto #${managedPullRequest.number} instead.
-    `,
-              })
-            );
-          } catch {
-            await context.octokit.issues.createComment(
-              context.repo({
-                issue_number: review.number,
-                body: `Invalid commit on review pull request! We reset the branch!
-
-    Please cherry-pick your commit ${commit.sha} onto #${managedPullRequest.number} instead.
-    `,
-              })
-            );
-          }
-        }
+        `update branch ${review}`,
+        synchronizeReview(context, review)
       );
     },
   });
--- src/github/commits.ts
+++ src/github/commits.ts
@@ -3,13 +3,17 @@ import { Context } from "probot";
 export async function getPullRequestCommits(
   context: Context,
   number: number
-): Promise<string[]> {
+): Promise<{ sha: string; author?: string }[]> {
   return await context.octokit.paginate(
     context.octokit.pulls.listCommits,
     context.repo({
       pull_number: number,
       per_page: 100,
     }),
-    ({ data: commits }) => commits.map((commit) => commit.sha)
+    ({ data: commits }) =>
+      commits.map((commit) => ({
+        sha: commit.sha,
+        author: commit.commit.author?.name,
+      }))
   );
 }
