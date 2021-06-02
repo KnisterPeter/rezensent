8ec70bdf6336d5a95828f0fccccd30075d1930f0
Markus Wolf <KnisterPeter@users.noreply.github.com>
2021-06-02 18:42:19 +0000

    chore: update .pnp.js

--- .pnp.js
+++ .pnp.js
@@ -7072,7 +7072,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ]],
       ["esbuild", [
         ["npm:0.11.15", {
-          "packageLocation": "./.yarn/cache/esbuild-npm-0.11.15-7b74182053-107341ba22.zip/node_modules/esbuild/",
+          "packageLocation": "./.yarn/unplugged/esbuild-npm-0.11.15-7b74182053/node_modules/esbuild/",
           "packageDependencies": [
             ["esbuild", "npm:0.11.15"]
           ],
